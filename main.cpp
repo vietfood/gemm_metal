71ce1f7dfea4e429ec732155acc0f170ba4cea79
Le Nguyen <lenguyen18072003@gmail.com>
2025-04-19 23:04:52 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,25 @@
 
 int main(int argc, char* argv[])
 {
+  assert(argc == 2);
+  std::string kernel_name = argv[1];
   BenchmarkMgr mgr;
-  mgr.run_opt2();
+
+  if (kernel_name == "naive") {
+    mgr.run_naive();
+  }
+
+  if (kernel_name == "opt_1") {
+    mgr.run_opt1();
+  }
+
+  if (kernel_name == "opt_2") {
+    mgr.run_opt2();
+  }
+
+  if (kernel_name == "opt_3") {
+    mgr.run_opt3();
+  }
+
   return 0;
 }
