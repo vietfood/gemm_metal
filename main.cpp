ef832f9ad806b5d743c080d57b442af752e3986f
Nguyen <lenguyen18072003@gmail.com>
2025-10-17 16:52:35 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char* argv[])
 {
-  // assert(argc == 2);
-  // std::string kernel_name = argv[1];
+  assert(argc == 2);
+  std::string kernel_name = argv[1];
 
   BenchmarkMgr mgr;
-  mgr.run_benchmark_suite("tile_16_unroll");
+  mgr.run_benchmark_suite(kernel_name);
 
   return 0;
 }
